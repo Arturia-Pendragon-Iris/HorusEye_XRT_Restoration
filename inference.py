3d4bb1357c112d127804b73dbe231b892d3f3d0c
Yuetan Chu <ytchu@mail.ustc.edu.cn>
2025-08-20 15:46:38 +0300

    Update inference.py

--- inference.py
+++ inference.py
@@ -5,39 +5,23 @@ from monai.inferers import SliceInferer, SlidingWindowInferer
 
 
 def predict_denoised_slice(ct_slice):
-    model = SwinUNet().cuda()
-    model.load_state_dict(torch.load("/data/Model/denoise_V10/HorusEye.pth"))
+    model = SwinUNet(in_ch=3).cuda()
     model.half()
     model.eval()
 
-    input_set = torch.tensor(np.stack([ct_slice] * 3, axis=0)[np.newaxis, :]).to(torch.float).to('cuda').half()
+    input_set = torch.from_numpy(np.stack((ct_slice, ct_slice, ct_slice), axis=0)[np.newaxis]).to(torch.float)
+    input_set = input_set.to('cuda').half()
+    model.load_state_dict(torch.load(
+        "/data/Model/HorusEye_demo.pth")) # replace the checkpoint path with your own path
+
     with torch.no_grad():
         inferer = SlidingWindowInferer(roi_size=(512, 512),
-                                       sw_batch_size=1,
+                                       sw_batch_size=4,
                                        overlap=0.25,
                                        mode="gaussian",
                                        sigma_scale=0.25,
                                        progress=False,
                                        sw_device="cuda",
                                        device="cpu")
-
         denoised = inferer(inputs=input_set, network=model).detach().cpu().numpy()[0, 0]
-    return np.array(np.mean(denoised, axis=0), "float32")
-
-
-def predict_denoised_volume(ct_array):
-    model = SwinUNet().cuda()
-    model.half()
-    model.eval()
-
-    input_set = torch.from_numpy(ct_array[np.newaxis, np.newaxis]).to(torch.float)
-    input_set = input_set.to('cuda').half()
-    model.load_state_dict(torch.load("/data/Model/denoise_V10/HorusEye.pth"))
-    with torch.no_grad():
-        inferer = SliceInferer(spatial_dim=2,
-                               roi_size=(512, 512),
-                               sw_batch_size=4,
-                               progress=False)
-        denoised = inferer(inputs=input_set, network=model).detach().cpu().numpy()[0, 0]
-
     return np.array(denoised, "float32")
